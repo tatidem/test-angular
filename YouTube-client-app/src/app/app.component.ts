98752df703d3bf3be6c2b8b11aff626df8a8ed47
tatidem <tdemyanova@mail.ru>
2024-07-04 13:16:03 +0400

    feat: add components and interfaces

--- YouTube-client-app/src/app/app.component.ts
+++ YouTube-client-app/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header/header.component';
+import { SearchItemComponent } from './search/search-item/search-item.component';
+import { SearchResultsComponent } from './search/search-results/search-results.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, SearchItemComponent, SearchResultsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
