02e06564791b40991d47c3e9e87d9011d2ed96c3
tatidem <tdemyanova@mail.ru>
2024-07-08 21:21:30 +0400

    feat: add card design

--- YouTube-client-app/src/app/search/search-item/search-item.component.ts
+++ YouTube-client-app/src/app/search/search-item/search-item.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { SearchItem } from '../search-item.model';
+import { ButtonComponent } from '../../sharedUI/button/button.component';
 
 @Component({
   selector: 'app-search-item',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './search-item.component.html',
   styleUrl: './search-item.component.scss',
 })
