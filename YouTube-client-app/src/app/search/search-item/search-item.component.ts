c66ef7b548cd7d4337dfc1fec07d15bb5f7e1e7b
tatidem <tdemyanova@mail.ru>
2024-07-08 22:39:56 +0400

    feat: implement ngClass and getcolor method

--- YouTube-client-app/src/app/search/search-item/search-item.component.ts
+++ YouTube-client-app/src/app/search/search-item/search-item.component.ts
@@ -1,14 +1,36 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { SearchItem } from '../search-item.model';
 import { ButtonComponent } from '../../sharedUI/button/button.component';
 
 @Component({
   selector: 'app-search-item',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, CommonModule],
   templateUrl: './search-item.component.html',
   styleUrl: './search-item.component.scss',
 })
 export class SearchItemComponent {
   @Input() item?: SearchItem;
+
+  getBorderColorClass(): string {
+    if (!this.item?.snippet.publishedAt) {
+      return '';
+    }
+
+    const currentDate = new Date();
+    const itemDate = new Date(this.item.snippet.publishedAt);
+    const differenceInDays = (currentDate.getTime() - itemDate.getTime()) / (1000 * 3600 * 24);
+
+    if (differenceInDays > 180) {
+      return 'border-red';
+    }
+    if (differenceInDays >= 30) {
+      return 'border-yellow';
+    }
+    if (differenceInDays >= 7) {
+      return 'border-green';
+    }
+    return 'border-blue';
+  }
 }
